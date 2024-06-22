12c454b60abb8496ef920b9be84398a0eaa68a9a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-22 01:18:19 -0500

    Add IUserRepository with GetUserByUsernameAsync and AddUserAsync methods

---
+++ CleanArchitecture_UserManagement.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture_UserManagement.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture_UserManagement.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByUsernameAsync(string username);
+        Task AddUserAsync(User user);
+    }
+}
