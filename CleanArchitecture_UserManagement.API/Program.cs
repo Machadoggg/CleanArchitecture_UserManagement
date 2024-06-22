b1d701ce4333c1cf7b016f2331151d627d695256
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-22 01:34:05 -0500

    Configuration program class to use layers

--- CleanArchitecture_UserManagement.API/Program.cs
+++ CleanArchitecture_UserManagement.API/Program.cs
@@ -1,6 +1,12 @@
+using CleanArchitecture_UserManagement.Application;
+using CleanArchitecture_UserManagement.Domain.Interfaces;
+using CleanArchitecture_UserManagement.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUserRepository, InMemoryUserRepository>();
+builder.Services.AddScoped<UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
