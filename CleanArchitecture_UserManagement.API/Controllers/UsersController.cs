bb428624621c22957d0641ff5f62d5b69333c73b
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-22 01:36:43 -0500

    Add controller for init session

---
+++ CleanArchitecture_UserManagement.API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using CleanArchitecture_UserManagement.Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture_UserManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            try
+            {
+                var user = await _userService.Login(request.Username, request.Password);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
+
