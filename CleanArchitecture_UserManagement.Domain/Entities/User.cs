8e4be6c0ab3da35703b2f1a7437a2dddcadb533a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-22 01:16:46 -0500

    Fix null defaults

--- CleanArchitecture_UserManagement.Domain/Entities/User.cs
+++ CleanArchitecture_UserManagement.Domain/Entities/User.cs
@@ -5,8 +5,8 @@ namespace CleanArchitecture_UserManagement.Domain.Entities
     public class User
     {
         public Guid Id { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-        public string Email { get; set; }
+        public string Username { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public string Email { get; set; } = default!;
     }
 }
